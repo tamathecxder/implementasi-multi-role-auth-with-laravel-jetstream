31279860362f9832dee2f570511737fd13907924
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-02-02 21:45:17 +0700

    Migrate all tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,12 +20,16 @@ return new class extends Migration
             $table->enum('role', [
                 'admin', 'kasir', 'owner'
             ]);
+            $table->unsignedBigInteger('id_outlet');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
+
+
+            $table->foreign('id_outlet')->references('id')->on('tb_outlet')->onDelete('cascade');
         });
     }
 
